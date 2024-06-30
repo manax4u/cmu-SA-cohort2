8f14500ab32220d6e606a3dc9d7e386506531145
Manjunath <manjunathswamy@gmail.com>
2024-06-30 12:03:43 +0530

    Update REST.js
    Delete order changes

--- ws/REST.js
+++ ws/REST.js
@@ -101,6 +101,25 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         });
     });
 
+    // Delete for /orders/order id specifier - deletes the order for the provided order ID
+    // req paramdter is the request object
+    // res parameter is the response object
+
+    router.delete("/orders/:order_id",function(req,res){
+        console.log("Deleting order ID: ", req.params.order_id );
+        var query = "DELETE FROM ?? WHERE ??=?";
+        var table = ["orders","order_id",req.params.order_id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+
+
 }
 
 // The next line just makes this module available... think of it as a kind package statement in Java
