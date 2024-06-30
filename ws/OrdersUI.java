52dd70de545bc6ec120bbd17e7ed3e62a7aead26
Manjunath <manjunathswamy@gmail.com>
2024-06-30 12:04:48 +0530

    Update OrdersUI.java
    Delete order changes

--- ws/OrdersUI.java
+++ ws/OrdersUI.java
@@ -57,7 +57,8 @@ public class OrdersUI
 			System.out.println( "Select an Option: \n" );
 			System.out.println( "1: Retrieve all orders in the order database." );
 			System.out.println( "2: Retrieve an order by ID." );
-			System.out.println( "3: Add a new order to the order database." );				
+			System.out.println( "3: Add a new order to the order database." );
+			System.out.println( "4: Delete an order by ID.");
 			System.out.println( "X: Exit\n" );
 			System.out.print( "\n>>>> " );
 			option = keyboard.next().charAt(0);	
@@ -188,6 +189,51 @@ public class OrdersUI
 				option = ' '; //Clearing option. This incase the user enterd X/x the program will not exit.
 
 			} // if
+
+			//////////// option 4 ////////////
+
+			if ( option == '4' )
+			{
+				// Here we get the order ID from the user
+
+				error = true;
+
+				while (error)
+				{
+					System.out.print( "\nEnter the order ID: " );
+					orderid = keyboard.nextLine();
+
+					try
+					{
+						Integer.parseInt(orderid);
+						error = false;
+					} catch (NumberFormatException e) {
+
+						System.out.println( "Not a number, please try again..." );
+						System.out.println("\nPress enter to continue..." );
+
+					} // if
+
+				} // while
+
+				try
+				{
+					response = api.retrieveOrders(orderid);
+					System.out.println(response);
+					response = api.deleteOrder(orderid);
+					System.out.println("Order " + orderid + " deleted!");
+
+				} catch (Exception e) {
+
+					System.out.println("Request failed:: " + e);
+
+				}
+
+				System.out.println("\nPress enter to continue..." );
+				c.readLine();
+
+			} // if
+
 
 			//////////// option X ////////////
 
