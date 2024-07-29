8c4e52bd0364b68d73f5e705a80ee8a60b066b3f
Paras Sachdeva <psachdeva@Parass-Laptop.local>
2024-07-29 21:53:43 +0530

    pulling latest changes

--- ws/REST.js
+++ ws/REST.js
@@ -20,6 +20,8 @@
 *   router.get("/orders"... - returns a listing of everything in the ws_orderinfo database
 *   router.get("/orders/:order_id"... - returns the data associated with order_id
 *   router.post("/order?"... - adds the new customer data into the ws_orderinfo database
+*   router.delete("/orders/:order_id"... - deletes order with order_id
+*   router.get("/authenticate/:userid"... - validates user credentials and returns user role
 *
 * External Dependencies: mysql
 *
@@ -42,6 +44,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // res parameter is the response object
 
     router.get("/",function(req,res){
+        console.log('Request Type:', req.method, ', Request: ', '/');
         res.json({"Message":"Orders Webservices Server Version 1.0"});
     });
 
@@ -50,14 +53,18 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // res parameter is the response object
 
     router.get("/orders",function(req,res){
-        console.log("Getting all database entries..." );
+        console.log('Request Type: ', req.method, ', Request: ', '/orders');
+        console.log("Getting all orders from database..." );
         var query = "SELECT * FROM ??";
         var table = ["orders"];
         query = mysql.format(query,table);
+        console.log('SQL query to get all orders: ', query);
         connection.query(query,function(err,rows){
             if(err) {
+                console.error('Error getting orders from database: ', err);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
+                console.log('GET Orders from database successfull...');
                 res.json({"Error" : false, "Message" : "Success", "Orders" : rows});
             }
         });
@@ -68,14 +75,19 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // res parameter is the response object
 
     router.get("/orders/:order_id",function(req,res){
-        console.log("Getting order ID: ", req.params.order_id );
+        const orderId = req.params.order_id;
+        console.log('Request Type: ', req.method, ', Request: ', '/orders/', orderId);
+        console.log("Getting order ID: ", orderId );
         var query = "SELECT * FROM ?? WHERE ??=?";
-        var table = ["orders","order_id",req.params.order_id];
+        var table = ["orders","order_id",orderId];
         query = mysql.format(query,table);
+        console.log('SQL query to get order ID ', orderId, ' is: ', query);
         connection.query(query,function(err,rows){
             if(err) {
+                console.error('Error getting order from database: ', err);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
+                console.log('GET Order from database successfull...');
                 res.json({"Error" : false, "Message" : "Success", "Users" : rows});
             }
         });
@@ -87,33 +99,42 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 
     router.post("/orders",function(req,res){
         //console.log("url:", req.url);
-        //console.log("body:", req.body);
+        console.log('Request Type: ', req.method, ', Request: ', '/orders');
+        console.log('Request Body: ', req.body);
         console.log("Adding to orders table ", req.body.order_date,",",req.body.first_name,",",req.body.last_name,",",req.body.address,",",req.body.phone);
         var query = "INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)";
         var table = ["orders","order_date","first_name","last_name","address","phone",req.body.order_date,req.body.first_name,req.body.last_name,req.body.address,req.body.phone];
         query = mysql.format(query,table);
+        console.log('SQL query to add order: ', query);
         connection.query(query,function(err,rows){
             if(err) {
+                console.error('Error adding order into database: ', err);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
+                console.log('Adding Order into database successfull...');
                 res.json({"Error" : false, "Message" : "User Added !"});
             }
         });
     });
 
     // Delete for /orders/order id specifier - deletes the order for the provided order ID
-    // req paramdter is the request object
+    // req parameter is the request object
     // res parameter is the response object
 
     router.delete("/orders/:order_id",function(req,res){
-        console.log("Deleting order ID: ", req.params.order_id );
+        const orderId = req.params.order_id;
+        console.log('Request Type: ', req.method, ', Request: ', '/orders/', orderId);
+        console.log("Deleting order ID: ", orderId );
         var query = "DELETE FROM ?? WHERE ??=?";
-        var table = ["orders","order_id",req.params.order_id];
+        var table = ["orders","order_id",orderId];
         query = mysql.format(query,table);
+        console.log('SQL query to delete order ID ', orderId, ' is: ', query);
         connection.query(query,function(err,rows){
             if(err) {
+                console.error('Error deleting order from database: ', err);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
+                console.log('DELETE Order from database successfull...');
                 res.json({"Error" : false, "Message" : "Success", "Users" : rows});
             }
         });
@@ -124,23 +145,24 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // res parameter is the response object
 
     router.get("/authenticate/:userid",function(req,res){
-        console.log("Validating credentials: ", req.params.userid );
+        const userId = req.params.userid;
+        console.log('Request Type: ', req.method, ', Request: ', '/authenticate/', userId);
+        console.log("Validating credentials: ", userId );
         var query = "SELECT password FROM ?? WHERE ??=?";;
-        var table = ["userCredential","user_name",req.params.userid];
+        var table = ["userCredential","user_name",userId];
         query = mysql.format(query,table);
+        console.log('SQL query to authenticate user ID ', userId, ' is: ', query);
         connection.query(query,function(err,rows){
             if(err) {
+                console.error('Error validating credentials from database: ', err);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
-
+                console.log('Validating credentials from database successfull...');
                 res.json({"Error" : false, "Message" : "Success", "password" : rows[0].password});
             }
         });
     });
-
-
 }
 
 // The next line just makes this module available... think of it as a kind package statement in Java
-
 module.exports = REST_ROUTER;
